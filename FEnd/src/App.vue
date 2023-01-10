af61800ccf3e793ff8ac40085f60d7ffc8dd2eb7
George Sidiropoulos <117164453+geosid4@users.noreply.github.com>
2023-01-10 21:59:37 +0200

    Add files via upload

--- FEnd/src/App.vue
+++ FEnd/src/App.vue
@@ -1,12 +1,63 @@
 <template>
-  <nav>
-    <router-link to="/home">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/statistics">Statistics</router-link>
+  <nav v-if="loggedIn">
+    <router-link to="/home">Αρχική</router-link> |
+    <router-link to="/statistics">Στατιστικά</router-link> |
+    <router-link to="/about">Αναζήτηση</router-link> | 
+    <router-link to="/qualify">Που περνάω</router-link> |
+    <a v-on:click="logout" :style="{ color: 'white', fontSize: '12px'}"> Logout </a>
+  </nav>
+  <nav v-else>
+    <router-link to="/home">Αρχική</router-link> | 
+    <router-link to="/statistics">Στατιστικά</router-link> |
+    <router-link to="/login" :style="{ color: 'white', fontSize: '12px'}">Login</router-link> -
+    <router-link to="/" :style="{ color: 'white', fontSize: '12px'}">SignUp</router-link> 
+  
+    
   </nav>
   <router-view/>
+  <PageUp />
 </template>
 
+<script>
+import PageUp from './components/PageUp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PageUp
+  },
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  created() {
+    this.isLoggedIn = localStorage.length>0;
+  },
+  methods:{
+    logout(){
+      localStorage.clear();
+      location.reload()
+      this.$router.push({name:'home'})
+    },
+    login2() {
+      this.isLoggedIn = true;
+      
+    },
+    logout2() {
+      this.isLoggedIn = false;
+      this.$router.push({name:'home'})
+      
+    },
+  },
+  computed: {
+    loggedIn() {
+      return this.isLoggedIn;
+    },
+  },
+  
+}
+</script>
 
 <style>
 
@@ -19,17 +70,18 @@
 }
 
 nav {
-  padding: 30px;
-  background-color: #009879;
-
-}
-
+    padding: 30px;
+    background-color: #009879;
+  
+  }
+  
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
+    font-weight: bold;
+    color: #2c3e50;
+  }
+  
 nav a.router-link-exact-active {
-  color: white;
-}
+    color: white;
+  }
+
 </style>
