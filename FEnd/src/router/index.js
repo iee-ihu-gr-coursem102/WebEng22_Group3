ae27252b8afa8f55b42da193dac87ed154c9b881
George Sidiropoulos <117164453+geosid4@users.noreply.github.com>
2023-01-10 22:00:13 +0200

    Add files via upload

--- FEnd/src/router/index.js
+++ FEnd/src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../components/views/HomeView.vue'
 import SignUp from '../components/SignUp'
-import StatisticsView from '../views/StatisticsView.vue'
+import SearchView from '../components/views/SearchView.vue'
+import StatisticsView from '../components/views/StatisticsView.vue'
+import LogIn from '../components/LogIn'
+import QuaLify from '../components/views/QuaLify.vue'
 
 const routes = [
   {
@@ -17,15 +20,26 @@ const routes = [
   {
     path: '/about',
     name: 'about',
+    component: SearchView
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    //component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
     path: '/statistics',
     name: 'statistics',
     component: StatisticsView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LogIn
+  },
+  {
+    path: '/qualify',
+    name: 'qualify',
+    component: QuaLify
   }
 ]
 
