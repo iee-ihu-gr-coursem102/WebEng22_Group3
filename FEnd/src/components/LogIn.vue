f343ff8a1099c12ab226671d53eb733a721e5e21
George Sidiropoulos <117164453+geosid4@users.noreply.github.com>
2023-01-15 19:30:33 +0200

    Add files via upload

--- FEnd/src/components/LogIn.vue
+++ FEnd/src/components/LogIn.vue
@@ -1,8 +1,8 @@
 <template>
     <h1> Login </h1>
     <div class="login">
-    <input type="text" v-model="email" placeholder="Enter Email" />
-    <input type="password" v-model="password" placeholder="Enter Password" />
+    <input type="text" v-model="email" placeholder="Enter Email"/>
+    <input type="password" v-model="password" placeholder="Enter Password"/>
     <button v-on:click="login"> Login </button>
 </div>
 
@@ -10,6 +10,8 @@
 
 <script>
 import axios from 'axios';
+import jwt_decode from "jwt-decode";
+//import jwt from "jsonwebtoken";
 
 export default{
     name:'LogIn',
@@ -21,18 +23,36 @@ export default{
     },
     methods:{
         async login(){
-            let result = await axios.get(
-                `http://localhost:3000/users?email=${this.email}&password=${this.password}` 
-            )
+            let result = await axios.post("http://ec2-18-184-43-109.eu-central-1.compute.amazonaws.com:5000/login",{
+                email:this.email,
+                password:this.password
+            });
+            // let result = await axios.get(
+            //     `http://ec2-18-184-43-109.eu-central-1.compute.amazonaws.com:5000/users?email=${this.email}&password=${this.password}` 
+            // )
+            console.log(result);
 
-            if(result.status==200 && result.data.length>0)
-            { 
-                localStorage.setItem("User-Info",JSON.stringify(result.data[0]))
-                this.$router.push({name:"home"})         
+            console.log(result.status==200);
+            console.log(result.data);
+
+            if(result.status==200)
+            {
+                
+                const token=result.data.token;
+                const decodedToken=jwt_decode(token);
+                console.log(decodedToken);
+                
+                localStorage.setItem("User-Info",JSON.stringify(decodedToken))
+                localStorage.setItem("User-Token",JSON.stringify(token))
             }
-            console.warn(result)
-            location.reload()
+
+                this.$router.push({name:"home"})         
+            
+            // console.warn(result)
+                location.reload()
+    
         }
+    
     },
     mounted(){
         let user= localStorage.getItem('User-Info');
@@ -43,5 +63,4 @@ export default{
     }
 }
 </script>
-
 
--- FEnd/src/components/SignUp.vue
+++ FEnd/src/components/SignUp.vue
@@ -1,3 +1,4 @@
+
 <template>
     <h1> Sign Up</h1>
 
@@ -5,13 +6,16 @@
     <input type="text" v-model="name" placeholder="Enter Name" />
     <input type="text" v-model="email" placeholder="Enter Email" />
     <input type="password" v-model="password" placeholder="Enter Password" />
+    <input type="password" v-model="password2" placeholder="Confirm Password" />
     <button v-on:click="signUp"> Sign Up </button>
 </div>
 
 </template>
 <script>
+//import jwt from "jsonwebtoken"
 
 import axios from 'axios'
+import jwt_decode from "jwt-decode";
 
 export default{
     name: 'SignUp',
@@ -21,27 +25,38 @@ export default{
         return {
             name:'',
             email:'',
-            password:''
+            password:'',
+            password2:''
         }
     },
 
     methods: {
         async signUp()
         {
-            let result = await axios.post("http://localhost:3000/users",{
+            try{
+            let result = await axios.post("http://ec2-18-184-43-109.eu-central-1.compute.amazonaws.com:5000/users",{
                 email:this.email,
                 password:this.password,
-                name:this.name
+                name:this.name,
+                confPassword:this.password2
             });
-            console.warn(result);
-            if(result.status==201)
+            console.log(result);
+            if (result.status==200)
             {
+                const token=result.data.token;
+                const decodedToken=jwt_decode(token);
+                console.log(decodedToken);
+                
+                localStorage.setItem("User-Info",JSON.stringify(decodedToken))
+                localStorage.setItem("User-Token",JSON.stringify(token))
+
                 // alert("Η εγγραφή σας ολοκληρώθηκε")   
-                localStorage.setItem("User-Info",JSON.stringify(result.data))
                 this.$router.push({name:"home"})
-                location.reload()         
-            }
-        }
+                location.reload()        
+            } 
+            
+        } catch(error){console.log(error)}
+    }
         },
     mounted(){
         let user= localStorage.getItem('User-Info');
